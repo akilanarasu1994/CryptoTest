8fafa74962fd83117da69ef7c08beb6dcedad43a
Akilan Arasu <akilan@redblacktree.com>
2017-06-26 19:02:15 +0530

    Keychain Sharing entitlement not working properly. Probably a signing identity issue

--- CryptoTest/ViewController.swift
+++ CryptoTest/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import SwiftyEC
 
 class ViewController: UIViewController {
 
+    let cskPri: String = "MEECAQAwEwYHKoZIzj0CAQYIKoZIzj0DAQcEJzAlAgEBBCANmGSJwsHKpNq7O9j218GsWaRGzNXc22kPJRx0YtvFXQ=="
+    
+//    var crypto: GMEllipticCurveCrypto = GMEllipticCurveCrypto.crypto(for: GMEllipticCurveSecp192r1) as! GMEllipticCurveCrypto
+    
+    var payload1: String = "29bef36d-ed42-4e4a-8a19-69491b6fef57:d32a212d-9ea6-4877-830e-0380ee2cc866:22ef4bd5-2768-46aa-9c40-c79e2f5481a6:1497955015490"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        do {
+        
+            let privateKey = try PrivateKey(base64Encoded: cskPri)
+            let payload1Clear = try ClearMessage(string: payload1, using: .utf8)
+            let signature = try payload1Clear.signed(with: privateKey, digestType: .sha256)
+            let signatureBase64 = signature.base64String
+            print(signatureBase64)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
